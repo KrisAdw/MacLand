baf95a3e7b0b7a9eb5240f908c7c1e700df8cb1a
Kris Adiwinata <krisadiwinata12@gmail.com>
2025-12-10 14:54:13 +0700

    Implement ProductViewer and added Canvas to the ProductViewer

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </>
   );
 };
